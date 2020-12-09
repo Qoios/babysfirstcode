b73fbec0b48f43c918b26213bd3f9e0074acf474
David B. Thomas <VaelynPhi@gmail.com>
2020-12-09 01:41:03 -0500

    Some web thingy.

--- test.js
+++ test.js
@@ -3,6 +3,14 @@
 // This is a comment.
 // This is not a comment.
 
-const fs = require("fs");
+// const fs = require("fs");
+const http = require('http');
 
-console.log("Whatever old nonsense.");
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Congrats this is a web page.");
+});
+
+server.listen(9001, "localhost", () => {
+	console.log(`Listening at http://localhost:9001/`);
+});
